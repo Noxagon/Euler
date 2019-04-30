4abebd9efbba247323fe86882a5a0a02ca37a48b
Noxagon <ignrnox@gmail.com>
2019-04-30 22:51:57 +0800

    added problem 7 solution

---
+++ Problems/Prob_7.py
@@ -0,0 +1,23 @@
+# Find the 10001st prime number
+import math
+
+prime = list()
+primeNum = 3
+
+prime.append(2)
+
+while len(prime) < 10001:
+    i = 0
+    check = True
+
+    while math.pow(prime[i], 2) <= primeNum:
+        if primeNum % prime[i] == 0:
+            check = False
+        i += 1
+
+    if check:
+        prime.append(primeNum)
+
+    primeNum += 2
+
+print("10001st prime number: " + str(prime[10000]))
